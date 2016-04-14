b52ab04747b1033dcc87ba80f89f6e810bb840e4
lmp <dev.larpon@gmail.com>
2016-04-14 23:18:16 +0200

    Added untested utility functions

--- Qak/js/utility.js
+++ Qak/js/utility.js
@@ -21,3 +21,25 @@ function randomIntFromInterval(min,max)
 {
     return Math.floor(Math.random()*(max-min+1)+min)
 }
+
+//Normalizes any number to an arbitrary range
+//by assuming the range wraps around when going below min or above max
+// NOTE untested
+function normalize( value, start, end )
+{
+    console.warn('Utility','using untested function normalize')
+    var width       = end - start
+    var offsetValue = value - start // value relative to 0
+
+    return ( offsetValue - ( Math.floor( offsetValue / width ) * width ) ) + start
+    // + start to reset back to start of original range
+}
+
+// NOTE untested
+function normalize0to360(degrees) {
+    console.warn('Utility','using untested function normalize0to360')
+    degrees = degrees % 360
+    if (degrees < 0)
+        degrees += 360
+    return degrees
+}
