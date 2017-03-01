c277ea541e404230b601eeeb12a1a7eb93143717
lmp <dev.larpon@gmail.com>
2017-03-01 18:19:11 +0100

    Added recursive Greatest Common Divisor function to Aid type

--- Qak/Tools/aid.js
+++ Qak/Tools/aid.js
@@ -226,6 +226,11 @@ function countPad(str, padChar) {
     return count
 }
 
+// Greatest Common Divisor
+function gcd (a, b) {
+    return (b === 0) ? a : gcd (b, a%b)
+}
+
 function distance(p1,p2) {
     return Math.sqrt( (p1.x-p2.x)*(p1.x-p2.x) + (p1.y-p2.y)*(p1.y-p2.y) );
 }
